61407fc420c2e1742df05b228e28528b5ed4cd33
Galina <koroleva2525.gk@gmail.com>
2022-11-09 07:10:26 +0100

    add task24

---
+++ Task24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число(A) и выдает сумму чисел от 1 до A
+// 7 ->28
+//4->10
+//8->36
+Console.WriteLine("Enter number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int sum = 0;
+while(i <= number)
+{
+    sum = sum + i;
+    i++;
+    
+    
+}
+ Console.WriteLine(sum);
